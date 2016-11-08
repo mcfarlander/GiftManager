77b59c7490f5a11f4c20d6337c8e29138ea19ba0
David Johnson <mcfarland_davej@davejgo2.local>
2016-11-08 05:01:57 -0600

    Initial Commit

---
+++ GiftManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GiftManager
+//
+//  Created by David Johnson on 11/8/16.
+//  Copyright © 2016 org.djohnson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
