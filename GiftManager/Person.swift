66e40f77ab33eacaf728a24b095ff64858f9ade0
David Johnson <mcfarland_davej@davejgo2.local>
2016-11-15 18:39:43 -0600

    initial commit

---
+++ GiftManager/Person.swift
@@ -0,0 +1,24 @@
+//
+//  Person.swift
+//  GiftManager
+//
+//  Created by David Johnson on 11/15/16.
+//  Copyright © 2016 org.djohnson. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Person: NSManagedObject
+{
+    @NSManaged var age: NSNumber?
+    @NSManaged var giftideas: String?
+    @NSManaged var ishousegift: NSNumber?
+    @NSManaged var ismaile: NSNumber?
+    @NSManaged var name: String?
+    @NSManaged var sequence: String?
+    
+    @NSManaged var house: NSObject?
+    @NSManaged var organization: NSObject?
+    
+}
