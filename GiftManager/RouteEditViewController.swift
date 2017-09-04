e66eb439ff3c1ea61ebbb629ebff43bfc8f371f5
David Johnson <mcfarland_davej@davejgo2.local>
2017-09-04 17:45:21 -0500

    initial commit

--- GiftManager/RouteEditViewController.swift
+++ GiftManager/RouteEditViewController.swift
@@ -8,27 +8,84 @@
 
 import Cocoa
 
-class RouteEditViewController: NSViewController
+class RouteEditViewController: NSWindowController
 {
+    
+    var mainW: NSWindow = NSWindow()
+    
     @IBOutlet weak var txtRouteNumber: NSTextField!
     @IBOutlet weak var txtStreetName: NSTextField!
     
     @IBOutlet weak var btnOk: NSButton!
     @IBOutlet weak var btnCancel: NSButton!
-
-    override func viewDidLoad()
+    
+    var okCancel = false
+    
+    var route:Route = Route()
+    var operation:DataOperation = DataOperation.Delete
+    
+    
+    override init(window: NSWindow!)
     {
-        super.viewDidLoad()
+        super.init(window: window)
     }
     
+    required init?(coder: (NSCoder!))
+    {
+        super.init(coder: coder);
+    }
+    
+    override func windowDidLoad()
+    {
+        super.windowDidLoad()
+        
+        txtRouteNumber.stringValue = route.routenumber!
+        txtStreetName.stringValue = route.street!
+        
+        switch operation
+        {
+        case DataOperation.Add:
+            txtRouteNumber.isEnabled = true
+            txtStreetName.isEnabled = true
+        case DataOperation.Update:
+            txtRouteNumber.isEnabled = true
+            txtStreetName.isEnabled = true
+        case DataOperation.Delete:
+            txtRouteNumber.isEnabled = false
+            txtStreetName.isEnabled = false
+        }
+    }
+
+
     @IBAction func btnOk_Action(_ sender: NSButton)
     {
+        self.route.routenumber = txtRouteNumber.stringValue
+        self.route.street = txtStreetName.stringValue
+        self.okCancel = true
+        endSheet()
+        
     }
     
     @IBAction func btnCancel_Action(_ sender: NSButton)
     {
+        okCancel = false
+        endSheet()
+        
     }
     
+    //method called to display the modal window
+    func beginSheet(mainWindow: NSWindow)
+    {
+        self.mainW = mainWindow
+        mainWindow.beginSheet(self.window!, completionHandler: nil)
+        
+    }
     
+    //method called to slide out the modal window
+    func endSheet()
+    {
+        self.mainW.endSheet(self.window!)
+        self.window!.orderOut(mainW)
+    }
     
 }
