84824bb72c16153598a8b6256f2caa5a72d34b0d
David Johnson <mcfarlander@charter.net>
2017-12-30 12:12:02 -0600

    added csv export class

---
+++ GiftManager/Common/CvsExport.swift
@@ -0,0 +1,104 @@
+//
+//  CvsExport.swift
+//  GiftManager
+//
+//  Created by Dave on 12/30/17.
+//  Copyright © 2017 org.djohnson. All rights reserved.
+//
+
+import Foundation
+
+protocol CvsExportDelegate
+{
+	func handleWroteCvs(success:Bool, filePath:String, errorMessage:String)
+}
+
+/**
+ A customer class to export data to a CSV file.
+ */
+class CvsExport
+{
+	fileprivate var path:String = ""
+	fileprivate var separator:String = ","
+	fileprivate var lines:[String] = [""]
+	
+	var delegate:CvsExportDelegate?
+	
+	init() { }
+	
+	init(path:String)
+	{
+		self.path = path
+	}
+	
+	/** Set the file path where the CSV file will be written to. */
+	func setPath(path:String) { self.path = path }
+	
+	/** Set the separator between items. Defaults to a comma. */
+	func setSeparator(separator:String) { self.separator = separator }
+	
+	/** Add a line to the file. No conversion of items is made. */
+	func appendLine(line:String) { self.lines.append(line) }
+	
+	/** Convert an array of strings to a single line, ready to be appended. */
+	func convertToLine(strings:[String], isHeader:Bool) -> String
+	{
+		var contents = ""
+		
+		for item:String in strings
+		{
+			if isHeader
+			{
+				contents.append("\"")
+			}
+			
+			contents.append(item)
+			
+			if isHeader
+			{
+				contents.append("\"")
+			}
+			
+			contents.append(self.separator)
+
+		}
+		
+		contents = String(contents.dropLast())
+		contents.append("\n")
+
+		return contents
+		
+	}
+	
+	/** Convert an array of strings to a single line and appends it to the array of lines immediately. */
+	func convertAndAppendLines(items:[String], isHeader:Bool)
+	{
+		let line = convertToLine(strings: items, isHeader: isHeader)
+		self.appendLine(line: line)
+	}
+	
+	/** Write the array of lines to file. */
+	func writeCvs()
+	{
+		let urlPath = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(self.path)
+		var csvText = ""
+		
+		for line in self.lines
+		{
+			csvText.append(line)
+		}
+		
+		do
+		{
+			try csvText.write(to: urlPath!, atomically: true, encoding: String.Encoding.utf8)
+			self.delegate?.handleWroteCvs(success: true, filePath: (urlPath?.absoluteString)!, errorMessage: "")
+		}
+		catch
+		{
+			self.delegate?.handleWroteCvs(success: false, filePath: "", errorMessage: "\(error)")
+		}
+		
+	}
+
+	
+}
