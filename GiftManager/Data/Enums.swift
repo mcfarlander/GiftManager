320f500f85987b83266f6e93d7c3a033ad9609bc
David Johnson <mcfarlander@charter.net>
2018-08-18 21:37:55 -0500

    added doc to data operation enum

--- GiftManager/Data/Enums.swift
+++ GiftManager/Data/Enums.swift
@@ -8,9 +8,17 @@
 
 import Foundation
 
+/// An enumeration of data operations
+///
+/// - Add: add a data element
+/// - Update: update a data element
+/// - Delete: delete a data slement
 enum DataOperation:Int
 {
+	/// Add a data element
     case Add
+	/// Update a data element
     case Update
+	/// Delete a data slement
     case Delete
 }
